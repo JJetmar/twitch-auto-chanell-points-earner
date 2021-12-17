a6106f1410c2991c2e1b42ee033f8d61f8fe56d1
JJetmar <JJetmar@users.noreply.github.com>
2021-12-17 09:30:10 +0100

    Create tampermonkey-twitch-auto-channel-points-earner.user.js

---
+++ tampermonkey-twitch-auto-channel-points-earner.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name         Twitch viewer point bonus auto-claimer
+// @namespace    https://github.com/JJetmar/twitch-auto-channel-points-earner
+// @version      0.1
+// @description  Tempermonkey extension used for automatic earning channel points on Twitch.tv platform.
+// @author       JJetmar
+// @match        https://www.twitch.tv/*
+// @icon         https://www.google.com/s2/favicons?domain=twitch.tv
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    setInterval(() => {
+        var claimBonusButton = document.querySelector(".claimable-bonus__icon")?.parentElement?.parentElement?.parentElement;
+
+        if (claimBonusButton) {
+            claimBonusButton.focus();
+            claimBonusButton.click();
+        }
+    }, 1000 * (1 + Math.random() * 10));
+})();
