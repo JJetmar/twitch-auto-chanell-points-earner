697f2198347e60db3fea7f70e000c8ee95e1aa8d
JJetmar <JJetmar@users.noreply.github.com>
2022-01-28 21:20:22 +0100

    feat: Added possibility to auto-refresh stream if it fails

--- tampermonkey-twitch-auto-channel-points-earner.user.js
+++ tampermonkey-twitch-auto-channel-points-earner.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Twitch viewer point bonus auto-claimer
 // @namespace    https://github.com/JJetmar/twitch-auto-channel-points-earner
-// @version      0.1
-// @description  Tempermonkey extension used for automatic earning channel points on Twitch.tv platform.
+// @version      0.2
+// @description  Tempermonkey extension used for automatic earning channel points on Twitch.tv platform and refreshing stream if it fails.
 // @author       JJetmar
 // @match        https://www.twitch.tv/*
 // @icon         https://www.google.com/s2/favicons?domain=twitch.tv
@@ -20,4 +20,9 @@
             claimBonusButton.click();
         }
     }, 1000 * (1 + Math.random() * 10));
+
+    setInterval(() => {
+        // Refresh stream if it fails
+        document.querySelector(".content-overlay-gate__allow-pointers button")?.click();
+    }, 1000);
 })();
